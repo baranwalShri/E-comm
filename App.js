68022d49a1e0eac28e6a06911ac946b03d4815e0
Ankit Kumar <ankit.kumar.mec19@itbhu.ac.in>
2023-08-27 15:14:19 +0530

    ignite episode

---
+++ App.js
@@ -0,0 +1,28 @@
+// const heading=React.createElement("h1",
+// {id:"heading",xyz:"xyz"},
+// "Hello world from React!");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading);//object
+// //console.log(root)
+// root.render(heading);
+
+
+/*
+   <div id="parent">
+       <div id="child"> 
+         <h1>I m h1 tag!</h1>
+       </div>
+   </div>
+Render helps us to put object into pages
+ReactElement(object)=>HTML(Browser Understands)
+
+*/
+
+const parent=React.createElement("div",
+{id:"parent"},[React.createElement("div",{id:"child1"},
+[React.createElement("h1",{},"I m h1 tag"),React.createElement("h2",{},"I m h2 tag")],React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"I m h1 tag"),React.createElement("h2",{},"I m h2 tag")]))
+]);
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
