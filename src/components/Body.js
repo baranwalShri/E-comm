aa504bebca7d54f8e48ecb5e3c986eb61a249fa8
Ankit Kumar <ankit.kumar.mec19@itbhu.ac.in>
2023-11-25 20:23:39 +0530

    c

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,7 +21,8 @@ const RestaurantsCardPromoted=withPromotedLabel(RestaurantCard);
   
   const  fetchData=async()=>{
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3412041&lng=82.99821510000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+      'https://corsproxy.io/?' + encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3412041&lng=82.99821510000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     ); 
     const json = await data.json();
     console.log("apiData",json);
